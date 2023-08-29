c3e5f87c60aedf98448f9390abeb294ac6fb1b2e
minhle <s3757324@rmit.edu.vn>
2023-08-29 14:03:13 +0700

    update role for mongodb

--- src/config/mongoconfig.js
+++ src/config/mongoconfig.js
@@ -1,6 +1,7 @@
 conn = Mongo();
 db = conn.getDB('admin');
 
+db.dropUser('root');
 db.createUser({
     user: 'root',
     pwd: 'JAAyBD9v7d9B8Y76', // or cleartext password
@@ -10,30 +11,43 @@ db.createUser({
     ],
 });
 
+db.dropUser('whadmin');
 db.createUser({
     user: 'whadmin',
     pwd: 'CnSNL2Dw50Hd9gui',
     roles: [{ role: 'readWrite', db: 'application' }],
 });
 
+db.dropUser('customer');
 db.createUser({
     user: 'customer',
     pwd: 'vVlOlqte0giTh1IQ',
     roles: [{ role: 'read', db: 'application' }],
 });
 
+try {
+    db.dropRole('staffRole');
+} catch (e) {
+    print(e);
+}
 db.createRole({
     role: 'staffRole',
     privileges: [
-        { resource: { db: 'application', collection: 'product' }, actions: ['insert, update', 'find'] },
+        { resource: { db: 'application', collection: 'product' }, actions: ['insert', 'update', 'find'] },
         { resource: { db: 'application', collection: 'category' }, actions: ['find'] },
     ],
+    roles: [{ role: 'read', db: 'application' }],
 });
 
+try {
+    db.dropUser('staff');
+} catch (e) {
+    print(e);
+}
 db.createUser({
     user: 'staff',
     pwd: 'vVlOlqte0giTh1IQ',
-    roles: [{ role: 'staffRole', db: 'application' }],
+    roles: ['staffRole'],
 });
 
 db.adminCommand({ shutdown: 1 });
